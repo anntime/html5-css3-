3a728e16745c6282fc77875d336414d59075d042
anntime <893191576@qq.com>
2019-01-15 16:31:22 +0800

    第十章例子

---
+++ js/89page.js
@@ -0,0 +1,20 @@
+var person1 = {
+	toLocaleString:function(){
+		return  "123";
+	},
+	toString:function(){
+		return "456";
+	}
+};
+var person2 = {
+	toLocaleString:function(){
+		return "789"
+	},
+	toString:function(){
+		return "000"
+    }
+};
+var people = [person1,person2];
+alert(people);
+alert(people.toString());
+alert(people.toLocaleString());
